e2779ac1568ca464e5451740061069fbd97dcecc
hbbhattarai <74581979+hbbhattarai@users.noreply.github.com>
2021-01-24 21:22:37 +0600

    blog 13

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use GrahamCampbell\Markdown\Facades\Markdown;
 
 class User extends Authenticatable
 {
