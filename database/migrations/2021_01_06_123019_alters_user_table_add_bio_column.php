100850687a1f9154ab269b5aa6e2a057d79a4fc6
hbbhattarai <74581979+hbbhattarai@users.noreply.github.com>
2021-01-24 21:11:16 +0600

    blog 13

--- database/migrations/2021_01_06_123019_alters_user_table_add_bio_column.php
+++ database/migrations/2021_01_06_123019_alters_user_table_add_bio_column.php
@@ -14,7 +14,7 @@ class AltersUserTableAddBioColumn extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('bio');
+            $table->text('bio')->nullable();
             $table->text('profile')->nullable();
         });
     }
